abab6d3b8a643c0a82eddd9a902de7843233c2a3
sami peralahti <sami.peralahti@vainu.io>
2018-08-26 13:04:17 -0700

    can crawl video urls

--- cfbvideo/cfbvideo/settings.py
+++ cfbvideo/cfbvideo/settings.py
@@ -16,7 +16,7 @@ NEWSPIDER_MODULE = 'cfbvideo.spiders'
 
 
 # Crawl responsibly by identifying yourself (and your website) on the user-agent
-#USER_AGENT = 'cfbvideo (+http://www.yourdomain.com)'
+USER_AGENT = 'Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/40.0.2214.85 Safari/537.36'
 
 # Obey robots.txt rules
 ROBOTSTXT_OBEY = True
@@ -27,7 +27,7 @@ ROBOTSTXT_OBEY = True
 # Configure a delay for requests for the same website (default: 0)
 # See https://doc.scrapy.org/en/latest/topics/settings.html#download-delay
 # See also autothrottle settings and docs
-#DOWNLOAD_DELAY = 3
+DOWNLOAD_DELAY = 3
 # The download delay setting will honor only one of:
 #CONCURRENT_REQUESTS_PER_DOMAIN = 16
 #CONCURRENT_REQUESTS_PER_IP = 16
--- cfbvideo/cfbvideo/spiders/videospider.py
+++ cfbvideo/cfbvideo/spiders/videospider.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 import scrapy
+from scrapy.http import Request
 
 
 class VideospiderSpider(scrapy.Spider):
@@ -8,4 +9,9 @@ class VideospiderSpider(scrapy.Spider):
     start_urls = ['https://www.channelfireball.com/videos/']
 
     def parse(self, response):
+        videos_urls = response.xpath('////*[@class="postTitle"]//a/@href').extract()
+        for video_url in videos_urls:
+            yield Request(video_url, callback= self.parse_video)
+
+    def parse_video(self, response):
         pass
