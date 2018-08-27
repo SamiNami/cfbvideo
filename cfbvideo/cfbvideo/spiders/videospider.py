61e5ab879ea065739741cc60d929ff9cf295d0e5
sami peralahti <sami.peralahti@vainu.io>
2018-08-26 18:44:21 -0700

    extcting tags

--- cfbvideo/cfbvideo/spiders/videospider.py
+++ cfbvideo/cfbvideo/spiders/videospider.py
@@ -8,6 +8,9 @@ class VideospiderSpider(scrapy.Spider):
     allowed_domains = ['channelfireball.com']
     start_urls = ['https://www.channelfireball.com/videos/']
 
+    def __init__(self):
+        self.count = 0
+
     def parse(self, response):
         videos_urls = response.xpath('////*[@class="postTitle"]//a/@href').extract()
         for video_url in videos_urls:
@@ -17,11 +20,26 @@ class VideospiderSpider(scrapy.Spider):
         head_data = response.xpath('//*[@class="postTitle"]')[0]
         head_data_list = head_data.xpath('.//text()')
 
-
         title = head_data_list[0].extract()
         author = head_data_list[2].extract()
         date = head_data_list[3].extract()
 
+        page_url = response.request.url
+        youtube_url = response.xpath('//*[@class="wp-video-shortcode"]//@src').extract_first()
 
+        avatar_url = response.xpath('//*[@class="gravatar"]//@src').extract_first()
+        bio_node =  response.xpath('//*[@class="about"]//child::*')[2]
+        bio = bio_node.xpath('.//text()').extract_first()
 
-        print("---------------------", response.request.url)
+        tags = response.xpath('//*[@rel="tag"]//text()').extract()
+
+        self.count += 1
+        print("---------------------",
+            self.count,
+            title,
+            author,
+            date,
+            page_url,
+            youtube_url,
+            avatar_url,
+            bio)
